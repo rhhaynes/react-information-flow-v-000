a3dfd7a1f2e01955908b966d408fad40a60753e0
R. Harris Haynes <harrishaynes@gmail.com>
2018-07-12 02:14:58 +0000

    Done.

--- src/Tier1.js
+++ src/Tier1.js
@@ -15,8 +15,6 @@ export default class Tier1 extends Component {
   }
 
   render() {
-    // hard coded color values have been added below, though they won't be
-    // present in our solution. What should they be replaced with?
     return (
       <div onClick={() => {this.setState({color: "#000"})}} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
         <Tier2 color={"#0F0"} />
