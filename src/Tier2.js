d16acf239a47f69483f863f8c2389a5e51990f60
R. Harris Haynes <harrishaynes@gmail.com>
2018-07-12 02:14:32 +0000

    automatically backed up by learn

--- src/Tier2.js
+++ src/Tier2.js
@@ -13,8 +13,6 @@ export default class Tier2 extends Component {
   }
 
   render() {
-    // hard coded color values have been added below, though they won't be
-    // present in our solution. What should they be replaced with?
     return (
       <div className="tier2" style={{backgroundColor: this.props.color, color: this.props.color}}>
         <Tier3 color={"#0F0"} />
